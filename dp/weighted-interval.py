84722235431e1d0887c9c352d9bc9d1a0a6e4cdb
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2025-03-07 17:35:53 -0700

    Use binary search to find prev compatible interval

--- dp/weighted-interval.py
+++ dp/weighted-interval.py
@@ -6,6 +6,8 @@ Output:
 An integer representing the maximum total weight of non-overlapping intervals.
 '''
 
+import math
+
 def weighted_interval_scheduling(intervals):    
     # sort intervals in increasing order of end times
     sortedIntervals = sorted(intervals, key = lambda t:t[1])
@@ -18,11 +20,37 @@ def weighted_interval_scheduling(intervals):
     for i in range(1, n):
         weight = sortedIntervals[i][2]
         # 2 options: include current interval or don't include it
-        dp[i] = max(weight + findLastCompatibleIntervalWeight(sortedIntervals, dp, i), dp[i-1]) 
+        dp[i] = max(weight + findLastCompatibleIntervalWeightBinary(sortedIntervals, dp, i), dp[i-1]) 
     
     return dp[n-1]
 
-def findLastCompatibleIntervalWeight(sortedIntervals, dp, i):
+
+
+# O(logn) for this algo, and O(n) * O(logn) for caller function
+def findLastCompatibleIntervalWeightBinary(sortedIntervals, dp, i):
+    targetStart = sortedIntervals[i][0]
+    left = 0
+    right = i-1
+    compatibleIntervalWeight = 0
+
+    while(True):
+        if(left > right):
+            break
+
+        mid = math.floor((left + right)/2)
+        currEnd = sortedIntervals[mid][1]
+
+        if(currEnd > targetStart):
+            right = mid - 1
+
+        if(currEnd <= targetStart):
+            compatibleIntervalWeight = dp[mid]
+            left = mid + 1
+
+    return compatibleIntervalWeight
+
+# O(n) for this algo, and O(n) * O(n) for caller function
+def findLastCompatibleIntervalWeightLinear(sortedIntervals, dp, i):
     currStart = sortedIntervals[i][0]
     for j in range(i-1, -1, -1):
         prevEnd = sortedIntervals[j][1]
